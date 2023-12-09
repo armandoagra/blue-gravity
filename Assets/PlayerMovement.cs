87408fda442af29879904f922c96d80821dc9086
Armando Agra <armando.agra@gmail.com>
2023-12-08 23:10:09 -0300

    Character animation

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,24 +7,41 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private LayerMask obstacleLayer;
     [SerializeField] private float speed = 5f;
+    [SerializeField] private Animator animator;
+    private int isWalkingHash; // For optimization
+    private bool isWalking;
     public Vector2 lookDirection { get; private set; } = Vector2.right; // Defaults to right
 
-
+    private void Start()
+    {
+        isWalkingHash = Animator.StringToHash("IsWalking");
+    }
     void Update()
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
 
-        Vector2 moveInput = new Vector2(h, v).normalized;
+        Vector2 moveInput = new Vector2(h, v);
         lookDirection = moveInput;
         if (moveInput != Vector2.zero)
         {
             if (!Physics2D.Raycast(transform.position, moveInput, 1f, obstacleLayer))
             {
                 // TODO: Let player "slide" by wall if pressing two keys but only one way is blocked
-                Vector2 moveVector = speed * Time.deltaTime * moveInput;
-                transform.Translate(moveVector);
+                // e.g.: there's a wall to the right, player is nearby, presses W+D --> goes up instead of not moving
+                Vector2 moveVector = speed * Time.deltaTime * moveInput.normalized;
+                if (moveVector.x < 0) transform.eulerAngles = new Vector2(0, 180);
+                else transform.eulerAngles = new Vector2(0, 0);
+
+                transform.Translate(moveVector, Space.World);
+                isWalking = true;
             }
         }
+        else
+        {
+            isWalking = false;
+        }
+        animator.SetBool(isWalkingHash, isWalking);
     }
+
 }
